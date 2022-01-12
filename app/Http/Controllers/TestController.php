1fad3b5511e3239e29ffea00a6cfad079c0378bf
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2022-01-12 10:26:06 +0900

    MVC　一連の流れを経験

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Test;
 
 class TestController extends Controller
 {
-    //
+    public function index(){
+
+        $values = Test::all();
+        // データが取れているか確認　dd(確認したい変数);
+
+        return view('tests.test', compact('values'));
+    }
 }
